34e33cd76e1579a5bd6b3f47f4a95dd36ce4a486
solomonc123 <jja2163@columbia.edu>
2022-12-02 23:34:43 +0000

    Refactored code and added new endpoints

--- src/main.py
+++ src/main.py
@@ -1,70 +1,13 @@
-import json
-from datetime import datetime
-
-from fastapi import FastAPI, Response, status, Request
-from fastapi.openapi.models import Response
-from fastapi.responses import JSONResponse
-
-from review_resource import ReviewResource
-
-app = FastAPI()
+import uvicorn
 
 
-@app.get("/api/v1/reviews/health")
-def get_health():
-    t = str(datetime.now())
-    # db_connection = check_db_connection()
-    msg = {
-        "name": "Review-Microservice",
-        "health": "Good",
-        "at time": t
-    }
-    result = Response(json.dumps(msg), status_code=status.HTTP_200_OK)
-    return result
+def start_reviews_microservice():
+    uvicorn.run(  # TODO: change later localhost..
+        app="app:app",
+        host="0.0.0.0",
+        port=5011
+    )
 
 
-@app.post("/api/v1/reviews")
-async def get_reviews_by_book_id(request: Request):
-    error = False
-    try:
-        data = await request.json()
-        result = ReviewResource.create_review(data["book_id"], data["review_text"], data["user_id"], data["score"])
-    except Exception as e:
-        result = {
-            "status": "Invalid Key Error",
-            "body": e
-        }
-        print("got an error")
-        error = True
-
-    if error:
-        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
-                            status_code=status.HTTP_400_BAD_REQUEST)
-    else:
-        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
-                            status_code=status.HTTP_200_OK)
-
-
-@app.get("/api/v1/reviews")
-def get_reviews_by_book_id(request: Request):
-    error = False
-    book_id, user_id = None, None
-    if 'book_id' in request.query_params:
-        book_id = request.query_params['book_id']
-    if 'user_id' in request.query_params:
-        user_id = request.query_params['user_id']
-    if book_id and user_id:
-        result = ReviewResource.get_by_book_and_user_id(book_id, user_id)
-    elif book_id:
-        result = ReviewResource.get_by_book_id(book_id)
-    elif user_id:
-        result = ReviewResource.get_by_user_id(user_id)
-    else:
-        result = ReviewResource.get_all_reviews()
-
-    if result:
-        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
-                            status_code=status.HTTP_200_OK)
-    else:
-        return JSONResponse(content='NOT FOUND',
-                            status_code=status.HTTP_404_NOT_FOUND)
+if __name__ == "__main__":
+    start_reviews_microservice()
---
+++ src/reviews_endpoint.py
@@ -0,0 +1,99 @@
+import json
+from datetime import datetime
+
+from fastapi import APIRouter, FastAPI, Response, status, Request
+from fastapi.openapi.models import Response
+from fastapi.responses import JSONResponse
+import uvicorn
+
+from review_resource import ReviewResource
+
+
+reviews_router = APIRouter(prefix='/api/v1/reviews')
+
+
+@reviews_router.get(path='/user_id/{user_id}', status_code=status.HTTP_200_OK, operation_id='get_all_reviews_by_user_id')
+def get_reviews_by_user_id(user_id: int):
+    """
+
+    :param user_id: user id
+    :return: return all reviews for a given user_id
+    """
+    result = ReviewResource.get_by_user_id(user_id)
+    if result:
+        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
+                            status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(content='NOT FOUND',
+                            status_code=status.HTTP_404_NOT_FOUND) 
+
+
+
+@reviews_router.get(path='/book_id/{book_id}', status_code=status.HTTP_200_OK, operation_id='get_all_reviews_by_book_id')
+def get_reviews_by_book_id_path(book_id: int):
+    """
+
+    :param book_id: book id
+    :return: return all reviews for a given book_id
+    """
+    result, avg  = ReviewResource.get_by_book_id(book_id)
+    msg = { 
+        "reviews": result, 
+        "average_score": avg
+    }
+    if result:
+        return JSONResponse(content=json.loads(json.dumps(msg, sort_keys=True, default=str)),
+                            status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(content='NOT FOUND',
+                            status_code=status.HTTP_404_NOT_FOUND) 
+    
+
+
+@reviews_router.post("")
+async def get_reviews_by_book_id(request: Request):
+    error = False
+    try:
+        data = await request.json()
+        result = ReviewResource.create_review(data["book_id"], data["review_text"], data["user_id"], data["score"])
+    except Exception as e:
+        result = {
+            "status": "Invalid Key Error",
+            "body": e
+        }
+        print("got an error")
+        error = True
+
+    if error:
+        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
+                            status_code=status.HTTP_400_BAD_REQUEST)
+    else:
+        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
+                            status_code=status.HTTP_200_OK)
+
+
+@reviews_router.get("")
+def get_reviews_by_book_id(request: Request):
+    error = False
+    book_id, user_id = None, None
+    if 'book_id' in request.query_params:
+        book_id = request.query_params['book_id']
+    if 'user_id' in request.query_params:
+        user_id = request.query_params['user_id']
+    if book_id and user_id:
+        result = ReviewResource.get_by_book_and_user_id(book_id, user_id)
+    elif book_id:
+        result = ReviewResource.get_by_book_id(book_id)
+    elif user_id:
+        result = ReviewResource.get_by_user_id(user_id)
+    else:
+        result = ReviewResource.get_all_reviews()
+
+    if result:
+        return JSONResponse(content=json.loads(json.dumps(result, sort_keys=True, default=str)),
+                            status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(content='NOT FOUND',
+                            status_code=status.HTTP_404_NOT_FOUND)
+
+
