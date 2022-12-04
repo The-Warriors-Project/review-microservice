8f68bfa2453fcdaf73938719cd7301df75895980
solomonc123 <jja2163@columbia.edu>
2022-12-04 23:03:26 +0000

    Added PUT functionality and changed user_id to username

--- src/app.py
+++ src/app.py
@@ -10,16 +10,24 @@ app.include_router(reviews_router)
 
 @app.get("/api/v1/reviews/health")
 def get_health():
-    connection = ReviewResource._get_connection()
-    t = str(datetime.now())
-    if connection:
-        db_connection_status = True
-        status_code = status.HTTP_200_OK
-        health = "Good"
-    else:
+    try:
+        connection = ReviewResource._get_connection()
+        t = str(datetime.now())
+        if connection:
+            db_connection_status = True
+            status_code = status.HTTP_200_OK
+            health = "Good"
+        else:
+            db_connection_status = False
+            status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+            health = "Not good"
+
+    except:
         db_connection_status = False
         status_code = status.HTTP_503_SERVICE_UNAVAILABLE
         health = "Not good"
+        t = str(datetime.now())
+
 
     msg = {
             "name": "Reviews-Microservice",
